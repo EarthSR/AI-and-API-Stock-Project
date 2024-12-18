35b9f71a1e30e1b21e2fd0839a5e79df5cd2ef70
EarthSR <earthsrichok31@gmail.com>
2024-12-19 02:05:27 +0700

    update

--- DNQ_Model/DNQ_model.py
+++ DNQ_Model/DNQ_model.py
@@ -256,11 +256,7 @@ def calculate_reward(action, current_price, next_price):
 def train_model(X_train, y_train, agent):
     total_rewards = []
     early_stopping = EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
-    model_checkpoint = ModelCheckpoint('best_model_DQN.h5', 
-                                   save_best_only=True, 
-                                   monitor='loss', 
-                                   mode='min',
-                                   save_format='h5')
+    model_checkpoint = ModelCheckpoint('best_model_DQN.keras', save_best_only=True, monitor='loss', mode='min')
     
     for epoch in range(CONFIG['EPOCHS']):
         if epoch % 5 == 0:
@@ -295,7 +291,7 @@ def train_model(X_train, y_train, agent):
         
         if (epoch + 1) % 10 == 0:
             plot_training_progress(total_rewards)
-            agent.model.save(f'model_checkpoint_epoch_{epoch+1}.h5', save_format='h5')
+            agent.model.save(f'model_checkpoint_epoch_{epoch+1}.keras')
         
         # ใช้ Early Stopping
         if early_stopping.on_epoch_end(epoch, logs={'loss': total_reward}):
@@ -618,7 +614,7 @@ def main():
         agent, total_rewards = train_model(X_train, y_train, agent)
         
         # Save trained model
-        agent.model.save('final_model_DQN.h5', save_format='h5')
+        agent.model.save('final_model_DQN.keras')
         
         # Plot training progress
         plot_training_progress(total_rewards)
