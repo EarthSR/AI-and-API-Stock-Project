2fb512618a61b27a434ae510c3a40b2d69328906
EarthSR <earthsrichok31@gmail.com>
2024-12-19 14:03:21 +0700

    update

--- MLP/MLP_model.py
+++ MLP/MLP_model.py
@@ -201,7 +201,7 @@ features_flat = Flatten()(features_input)
 merged = concatenate([features_flat, ticker_embedding_flat])
 
 # ลด Dense Layer เหลือ 2 Layers
-x = Dense(128, activation='relu')(merged)
+x = Dense(64, activation='relu')(merged)
 x = Dropout(0.3)(x)  # Dropout หลัง Dense Layer แรก
 x = Dense(32, activation='relu')(x)
 output = Dense(1)(x)  # Output Layer
